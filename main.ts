714a0e4a1839a3cc041bfe9d8c3a1b7081524224
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-09-19 11:27:16 -0400

    created program to show temperature & corrected README.md, name & date

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Peter Zerbinos
+ * Created on: Sep 2024
+ * This program shows the temperature
 */
 
-basic.showString('Hello, World!')
+//happy face when turned on
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//our variable for the temperature
+let findngTemperature = input.temperature()
+
+//pressing A will show temperature then end with happy face
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString('The temperature is:')
+    basic.showNumber(findngTemperature)
+    basic.showString('C.')
+    basic.showIcon(IconNames.Happy)
+})
