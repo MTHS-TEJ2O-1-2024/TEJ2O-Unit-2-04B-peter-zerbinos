5cbb123643183d0d2f00672c39bee2eeb0682f7a
Peter Zerbinos <peter.zerbinos@mths.ca>
2024-09-20 15:20:18 +0000

    edited MicroPython code & made hex file

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,27 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Copyright (c) 2020 MTHS All rights reserved
+
+Created by: Peter Zerbinos
+Created on: Sep 2024
+This program shows the temperature
+
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# happy face when turned on
+display.clear()
+display.show(Image.HAPPY)
+
+# our variable for the temperature
+findingTemperature = input.temperature()
+
+# pressing A will show temperature then end with happy face
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        display.scroll("The temperature is:")
+        display.scroll(str(findingTemperature))
+        display.scroll("C.")
+        display.show(Image.HAPPY)
